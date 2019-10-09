ace397ae7b48a00ceb0a926fa119d65b82654de1
Rohit Pujari <rohitspujari@gmail.com>
2019-10-08 19:55:41 -0500

    changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -15,8 +15,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "https://speakintongues.sonasher.com/",
-        "redirectSignOut": "https://speakintongues.sonasher.com/",
+        "redirectSignIn": "http://localhost:3000/",
+        "redirectSignOut": "http://localhost:3000/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
